1703390d55a342c9458076a89c3bf4a8a62d254c
ayeshNipun <nipun.yesh@gmail.com>
2018-06-22 11:39:02 +0530

    navbar

--- code/partials/navbar.php
+++ code/partials/navbar.php
@@ -1,4 +1,105 @@
-<nav class="navbar navbar-default" role="navigation">
+ 
+
+<div>
+<ul class="navul" id="navBar">
+	<div class="flleft">
+		<li class="navItem title"><a href="../main/index.php"><span class="navHead1">S</span><span class="navHead2">eekers</span></a></li>
+	</div>
+	  
+	<div class="flright">
+		<li class="navItem"><a href="../main/index.php">Home</a></li>
+  		<li class="navItem"><a href="../main/contact.php">Contact</a></li>
+		<li class="navItem"><a href="../main/about.php">About</a></li>
+		<li class="navItem"><a href="../main/contact.php">Register</a></li>
+  		<li class="navItem"><a href="../main/about.php">Login</a></li>
+	</div>
+</ul>
+</div>
+<div class="clearfix"></div>
+
+
+<style>
+	
+.navul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #ff704d;
+}
+
+.title{
+	margin-top:-5px;
+}
+
+.navHead1{
+	font-size:30px;
+}
+
+.navHead2{
+	font-size:20px !important;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+    background-color: #111;
+}
+
+.active {
+    background-color: #4CAF50;
+}
+
+.flleft{
+    margin-left: 150px;
+}
+.flright{
+	margin-top:5px;
+    margin-left: 1100px;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <nav class="navbar navbar-default" role="navigation">
 	<div class="container">
 	    <div class="navbar-header">
 	        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -8,8 +109,8 @@
 		        <span class="icon-bar"></span>
 	        </button>
 	        <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt=""/></a>
-	    </div>
-	    <!--/.navbar-header-->
+		</div>
+		
 	    <div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1" style="height: 1px;">
 	        <ul class="nav navbar-nav">
 		        <li class="dropdown">
@@ -94,5 +195,4 @@
 	    </div>
 	    <div class="clearfix"> </div>
 	  </div>
-	    <!--/.navbar-collapse-->
-	</nav>
+	</nav> -->
